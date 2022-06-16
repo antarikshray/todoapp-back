a764faa13323424c01397e882660b421b707f70d
antarikshray <antarikshray@gmail.com>
2022-06-16 05:50:17 +0530

    [service] add service to get list of todos

--- src/main/java/com/tw/todo/service/TodoService.java
+++ src/main/java/com/tw/todo/service/TodoService.java
@@ -1,6 +1,15 @@
 package com.tw.todo.service;
 
+import com.tw.todo.model.Todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TodoService {
 
+    private List<Todo> todoList = new ArrayList<>();
 
+    public List<Todo> getListOfTodos() {
+        return todoList;
+    }
 }
